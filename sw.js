9ebc19cc0f26b5aae5f1290db9ec9c40b46ba7bd
fersousa00 <fersousa00@gmail.com>
2026-05-14 20:55:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'antifragil-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'antifragil-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
