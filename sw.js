095a3a6f128b2d7a87bbbef73042bdf1111df750
fersousa00 <fersousa00@gmail.com>
2026-05-14 21:16:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-const CACHE = 'antifragil-v3';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+// Development hotfix: no cache. It also removes old caches.
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
 });
